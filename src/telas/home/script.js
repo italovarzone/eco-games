b868feefbab48fb1fb9c94d8aa20e4fed498a02c
Icaro Lira <icarolira895@gmail.com>
2024-08-07 10:55:03 -0300

    fix: home bug in merge

--- src/telas/home/script.js
+++ src/telas/home/script.js
@@ -1,96 +1,85 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const sidebar = document.querySelector('.sidebar');
-    handleSidebarVisibility(); // Inicialmente, verificar o tamanho da tela e ajustar a sidebar
+  const sidebar = document.querySelector('.sidebar');
+  handleSidebarVisibility(); // Inicialmente, verificar o tamanho da tela e ajustar a sidebar
 
-    window.addEventListener('resize', handleSidebarVisibility); // Adicionar listener para redimensionamento da tela
+  window.addEventListener('resize', handleSidebarVisibility); // Adicionar listener para redimensionamento da tela
 });
 
 function handleSidebarVisibility() {
-    const sidebar = document.querySelector('.sidebar');
-    if (window.innerWidth < 1000) {
-        sidebar.classList.remove('open');
-    } else {
-        sidebar.classList.add('open');
-    }
+  const sidebar = document.querySelector('.sidebar');
+  if (window.innerWidth < 1000) {
+      sidebar.classList.remove('open');
+  } else {
+      sidebar.classList.add('open');
+  }
 }
 
 function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('open');
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('open');
 }
 
 function logout() {
-    window.location.href = 'logout.html';
+  window.location.href = 'logout.html';
 }
 
-    liGoToPerfil.addEventListener('click', () => {
-        window.location.href = '../perfil/index.html';
-    });
-
-    gameOptions.forEach(option => {
-        option.addEventListener('click', () => {
-            const game = option.getAttribute('data-game');
-            loadGame(game);
-        });
-    });
-
-    function loadGame(game) {
-        const gameContainer = document.getElementById('game-container');
-        const sidebar = document.querySelector('.sidebar');
-        const content = document.querySelector('.content'); 
-        const sidebarList = document.querySelector('.sidebar-list');
-
-        gameContainer.innerHTML = '';
-
-        switch (game) {
-            case 'cacapalavras':
-                loadCacaPalavras();
-                break;
-            case 'hangame':
-                loadHangame();
-                break;
-            case 'ecopuzzle':
-                loadEcopuzzle();
-                break;
-            case 'quiz':
-                loadQuizODS();
-                break;
-            default:
-                gameContainer.innerHTML = '<h2>Selecione um jogo na barra lateral</h2>';
-        }
-
-    // Fechar a sidebar após a seleção do jogo
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.remove('open');
+function goToPerfil() {
+  window.location.href = '../perfil/index.html'; 
 }
 
-    function loadCacaPalavras() {
-        const gameContainer = document.getElementById('game-container');
-        gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="../../jogos/cacapalavras/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
-    `;
-    }
+function loadGame(game) {
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.innerHTML = '';
 
-    function loadHangame() {
-        const gameContainer = document.getElementById('game-container');
-        gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="../../jogos/forca/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
-    `;
-    }
+  switch (game) {
+      case 'cacapalavras':
+          loadCacaPalavras();
+          break;
+      case 'hangame':
+          loadHangame();
+          break;
+      case 'ecopuzzle':
+          loadEcopuzzle();
+          break;
+      case 'quiz':
+          loadQuizODS();
+          break;
+      default:
+          gameContainer.innerHTML = '<h2>Selecione um jogo na barra lateral</h2>';
+  }
 
-    function loadEcopuzzle() {
-        const gameContainer = document.getElementById('game-container');
-        gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="../../jogos/ecopuzzle/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
-    `;
-    }
+  // Fechar a sidebar após a seleção do jogo
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.remove('open');
+}
 
-    function loadQuizODS() {
-        const gameContainer = document.getElementById('game-container');
-        gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="../../jogos/quiz/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
-    `;
-    }
+function loadCacaPalavras() {
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.innerHTML = `
+      <iframe id="game-iframe" src="../../jogos/cacapalavras/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+  `;
+}
+
+function loadHangame() {
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.innerHTML = `
+      <iframe id="game-iframe" src="../../jogos/forca/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+  `;
+}
+
+function loadEcopuzzle() {
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.innerHTML = `
+      <iframe id="game-iframe" src="../../jogos/ecopuzzle/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+  `;
+}
+
+function loadQuizODS() {
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.innerHTML = `
+      <iframe id="game-iframe" src="../../jogos/quiz/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+  `;
+}
 
 let currentSlide = 0;
 let startX;
@@ -98,92 +87,92 @@ let isDragging = false;
 let slideInterval;
 
 function showSlide(slideIndex, carousel) {
-  const slides = document.querySelectorAll(carousel + ' .carousel-item');
-  const indicators = document.querySelectorAll('.carousel-indicators .indicator');
+const slides = document.querySelectorAll(carousel + ' .carousel-item');
+const indicators = document.querySelectorAll('.carousel-indicators .indicator');
 
-  if (slideIndex >= slides.length) {
-    currentSlide = 0;
-  } else if (slideIndex < 0) {
-    currentSlide = slides.length - 1;
-  } else {
-    currentSlide = slideIndex;
+if (slideIndex >= slides.length) {
+  currentSlide = 0;
+} else if (slideIndex < 0) {
+  currentSlide = slides.length - 1;
+} else {
+  currentSlide = slideIndex;
+}
+
+slides.forEach((slide, index) => {
+  slide.style.transform = `translateX(${-currentSlide * 100}%)`;
+});
+
+indicators.forEach((indicator, index) => {
+  indicator.classList.remove('active');
+  if (index === currentSlide) {
+    indicator.classList.add('active');
   }
-  
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${-currentSlide * 100}%)`;
-  });
-
-  indicators.forEach((indicator, index) => {
-    indicator.classList.remove('active');
-    if (index === currentSlide) {
-      indicator.classList.add('active');
-    }
-  });
+});
 }
 
 function nextSlide() {
-  const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
-  showSlide(currentSlide + 1, carousel);
+const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
+showSlide(currentSlide + 1, carousel);
 }
 
 function goToSlide(slideIndex) {
-  const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
-  showSlide(slideIndex, carousel);
+const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
+showSlide(slideIndex, carousel);
 }
 
 function startSlideShow() {
-  slideInterval = setInterval(nextSlide, 5000);
+slideInterval = setInterval(nextSlide, 5000);
 }
 
 function stopSlideShow() {
-  clearInterval(slideInterval);
+clearInterval(slideInterval);
 }
 
 function handleTouchStart(event) {
-  startX = event.touches[0].clientX;
-  isDragging = true;
-  stopSlideShow();
+startX = event.touches[0].clientX;
+isDragging = true;
+stopSlideShow();
 }
 
 function handleTouchMove(event) {
-  if (!isDragging) return;
-  const currentX = event.touches[0].clientX;
-  const diffX = startX - currentX;
+if (!isDragging) return;
+const currentX = event.touches[0].clientX;
+const diffX = startX - currentX;
 
-  const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
-  if (diffX > 50) {
-    nextSlide();
-    isDragging = false;
-  } else if (diffX < -50) {
-    currentSlide--;
-    const slides = document.querySelectorAll(carousel + ' .carousel-item');
-    currentSlide = currentSlide < 0 ? slides.length - 1 : currentSlide;
-    showSlide(currentSlide, carousel);
-    isDragging = false;
-  }
+const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
+if (diffX > 50) {
+  nextSlide();
+  isDragging = false;
+} else if (diffX < -50) {
+  currentSlide--;
+  const slides = document.querySelectorAll(carousel + ' .carousel-item');
+  currentSlide = currentSlide < 0 ? slides.length - 1 : currentSlide;
+  showSlide(currentSlide, carousel);
+  isDragging = false;
+}
 }
 
 function handleTouchEnd() {
-  isDragging = false;
-  startSlideShow();
+isDragging = false;
+startSlideShow();
 }
 
 // Inicializa o carrossel ao carregar a página
 window.addEventListener('load', () => {
-  const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
-  showSlide(currentSlide, carousel);
-  startSlideShow();
+const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
+showSlide(currentSlide, carousel);
+startSlideShow();
 
-  const carousels = document.querySelectorAll('.carousel');
-  carousels.forEach(carousel => {
-    carousel.addEventListener('touchstart', handleTouchStart);
-    carousel.addEventListener('touchmove', handleTouchMove);
-    carousel.addEventListener('touchend', handleTouchEnd);
-  });
+const carousels = document.querySelectorAll('.carousel');
+carousels.forEach(carousel => {
+  carousel.addEventListener('touchstart', handleTouchStart);
+  carousel.addEventListener('touchmove', handleTouchMove);
+  carousel.addEventListener('touchend', handleTouchEnd);
+});
 });
 
 // Atualiza o carrossel ao redimensionar a janela
 window.addEventListener('resize', () => {
-  const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
-  showSlide(currentSlide, carousel);
+const carousel = window.innerWidth >= 1000 ? '.desktop-carousel' : '.mobile-carousel';
+showSlide(currentSlide, carousel);
 });
