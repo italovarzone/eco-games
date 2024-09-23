2511881aba55191a91c8bc5dbebb9a013918d094
weltoonmoreira <106163024+weltoonmoreira@users.noreply.github.com>
2024-09-23 10:00:29 -0200

    feat: acerto branch

--- src/telas/ranking/script.js
+++ src/telas/ranking/script.js
@@ -1,14 +1,4 @@
-import { getCrosswoldRanking } from '../../services/ranking/crossworld.js';
-import { getEcopuzzleRanking } from '../../services/ranking/ecopuzzle.js';
-import { getHangameRanking } from '../../services/ranking/hangame.js';
-import { getQuizRanking } from '../../services/ranking/quiz.js';
-
-document.addEventListener('DOMContentLoaded', async function () {
-    const numeroTabCrossworld = 1;
-    await handleRanking(numeroTabCrossworld);
-    
-    const btnGoToMenu = document.getElementById('btnGotoMenu');
-
+document.addEventListener('DOMContentLoaded', function () {
     const tabs = document.querySelectorAll('.tab-header .tab');
     const contents = document.querySelectorAll('.tab-content .content');
     const slider = document.querySelector('.mobile-slider');
@@ -16,13 +6,12 @@ document.addEventListener('DOMContentLoaded', async function () {
     // Lógica de abas para desktop
     if (window.innerWidth > 768) {
         tabs.forEach(tab => {
-            tab.addEventListener('click', async function () {
+            tab.addEventListener('click', function () {
                 tabs.forEach(t => t.classList.remove('active'));
                 tab.classList.add('active');
                 contents.forEach(content => content.classList.remove('active'));
                 const tabNumber = tab.getAttribute('data-tab');
                 document.getElementById(`tab-${tabNumber}`).classList.add('active');
-                await handleRanking(tabNumber);
             });
         });
     } else {
@@ -46,78 +35,8 @@ document.addEventListener('DOMContentLoaded', async function () {
             isDragging = false;
         });
     }
-
-    btnGoToMenu.addEventListener('click', () => {
-        window.location.href = '../home/index.html';
-      });
 });
 
-async function handleRanking(tabNumber) {
-    let ranking;
-    switch (parseInt(tabNumber)) {
-        case 1:
-            ranking = await getCrosswoldRanking(); 
-            console.log(ranking);
-            break;
-
-        case 2:
-            ranking = await getEcopuzzleRanking();
-            console.log(ranking);
-            break;
-        case 3:
-            ranking = await getHangameRanking();
-            console.log(ranking);
-            break;
-        case 4:
-            ranking = await getQuizRanking();
-            console.log(ranking);
-            break;
-    }
-
-    if (ranking) {
-        let user = ranking.shift();
-        console.log(user)
-        updateRankingTable(ranking, tabNumber, user);
-    }
-
-    function updateRankingTable(rankingData, tabNumber, user) {
-
-        // Seleciona o tbody da tabela específica
-        const tableBody = document.querySelector(`#tab-${tabNumber} .ranking-table tbody`);
-        
-        // Limpa qualquer conteúdo anterior da tabela
-        tableBody.innerHTML = '';
-
-        const userPositionElement = document.querySelector(`#tab-${tabNumber} .score-user-actual .player p:nth-child(1)`);
-        const userNameElement = document.querySelector(`#tab-${tabNumber} .score-user-actual .player p:nth-child(2)`);
-        const userScoreElement = document.querySelector(`#tab-${tabNumber} .score-user-actual .player p:nth-child(3)`);
-    
-        userPositionElement.textContent = user.posicao;
-        userNameElement.textContent = user.nome;
-        userScoreElement.textContent = (user.tempo / 1000);
-        // Itera sobre os dados de ranking e cria as linhas da tabela
-        rankingData.forEach(player => {
-            const row = document.createElement('tr');
-    
-            // Adiciona a posição do jogador
-            const positionCell = document.createElement('td');
-            positionCell.textContent = player.posicao;
-            row.appendChild(positionCell);
-    
-            // Adiciona o nome do jogador
-            const nameCell = document.createElement('td');
-            nameCell.textContent = player.nome;
-            row.appendChild(nameCell);
-    
-            // Adiciona a pontuação/tempo do jogador
-            const scoreCell = document.createElement('td');
-            scoreCell.textContent = player.tempo / 1000;
-            row.appendChild(scoreCell);
-    
-            // Insere a linha na tabela
-            tableBody.appendChild(row);
-        });
-    }
+function goToPerfil() {
+    window.location.href = '../perfil/index.html'; 
 }
-
-
