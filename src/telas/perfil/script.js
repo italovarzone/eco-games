181eafd6d5c93b1dadff79753a4f80228fab53f1
Icaro Lira <icarolira895@gmail.com>
2024-10-16 08:35:49 -0300

    fix: perfil poucas infomacoes bug

--- src/telas/perfil/script.js
+++ src/telas/perfil/script.js
@@ -34,10 +34,10 @@ window.onload = function() {
 const updateUI = (info) => {
   const totalTime = convertSecondsToMinutesAndSeconds(info.baseInfo.tempo_total_jogos / 1000)
   document.querySelector('.perfil-info h2').innerText = info.baseInfo.usuario_nome;
-  document.getElementById('spanCrossworldPosition').innerText = info.positions.crossworld.posicao
-  document.getElementById('spanHangamePosition').innerText = info.positions.hangame.posicao
-  document.getElementById('spanGreenGeniusPosition').innerText = info.positions.quiz.posicao
-  document.getElementById('spanEcopuzzlePosition').innerText = info.positions.ecopuzzle.posicao
+  document.getElementById('spanCrossworldPosition').innerText = info.positions.crossworld? info.positions.crossworld.posicao : "--"
+  document.getElementById('spanHangamePosition').innerText = info.positions.hangame? info.positions.hangame.posicao : "--"
+document.getElementById('spanGreenGeniusPosition').innerText = info.positions.quiz? info.positions.quiz.posicao : "--"
+  document.getElementById('spanEcopuzzlePosition').innerText = info.positions.ecopuzzle? info.positions.ecopuzzle.posicao : "--"
   document.getElementById('spanJogoMaisJogado').innerText = info.baseInfo.jogo_mais_jogado;
   document.getElementById('spanDesafiosVencidos').innerText = info.baseInfo.desafios_vencidos;
   document.getElementById('spanTempoTotal').innerText = totalTime;
