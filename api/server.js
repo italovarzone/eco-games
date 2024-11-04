ad080d0ef9d71e317ac8c27e1ef6e3e2eeae5454
Italo Pescador Varzone <ipv@vimansca.com.br>
2024-11-04 08:13:19 -0300

    feat: ajusta cors novo.

--- api/server.js
+++ api/server.js
@@ -13,12 +13,12 @@ const axios = require('axios');
 const app = express();
 
 app.use(cors({
-  origin: 'https://sustenteco-app.onrender.com', 
+  origin: 'https://sustenteco.com', 
   credentials: true
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sustenteco-app.onrender.com"); 
+  res.header("Access-Control-Allow-Origin", "https://sustenteco.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
